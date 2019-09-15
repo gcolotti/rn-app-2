eaceb3242851ae964194595780a12c53cb20787e
Guillermo Colotti <guille.theoffspring@gmail.com>
2019-09-15 15:08:18 -0300

    Added some style to GameOverScreen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { View, Button, Text, StyleSheet } from 'react-native';
+import Colors from './../constants/colors';
 
 const GameOverScreen = props => {
     return (
         <View style={styles.screen}>
-            <Text>The Game is Over!</Text>
+            <Text style={styles.text}>The Game is Over!</Text>
+            <Text style={styles.text}>Number of rounds: {props.roundsNumber}</Text>
+            <Text style={styles.text}>Number was: {props.userNumber}</Text>
+            <View style={styles.btnContainer}>
+                <Button color={Colors.primary}title={'New Game'} onPress={props.onNewGame} />
+            </View>
         </View>
     );
 };
@@ -14,6 +20,13 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    text: {
+        fontSize: 20,
+    },
+    btnContainer: {
+        width: '80%',
+        marginTop: 20,
     }
 });
 
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -9,9 +9,8 @@ const generateRandomBetween = (min, max, exclude) => {
     const rndNum = Math.floor(Math.random() * (max - min)) + min;
     if(rndNum === exclude) {
         return generateRandomBetween(min, max, exclude);
-    } else {
-        return rndNum;
     }
+    return rndNum;
 };
 
 const GameScreen = props => {
